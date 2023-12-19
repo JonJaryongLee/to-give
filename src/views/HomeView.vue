5dc688a6ef49e9cad31ae59e3e6ae974c0c21c8f
JonJaryongLee <director9303@gmail.com>
2023-12-19 16:40:13 +0900

    props prepared

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <script setup>
 import HomeChild from "@/components/HomeChild.vue";
+import { ref } from "vue";
+
+const name = ref("david");
+const age = ref(10);
 </script>
 
 <template>
   <h1>HomeView</h1>
+  <p>{{ name }}</p>
+  <p>{{ age }}</p>
   <HomeChild />
 </template>
