4a425fca5b2ded8441e6f95ccd385873d55d5f49
JonJaryongLee <director9303@gmail.com>
2023-12-20 16:07:56 +0900

    firebase finished

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,49 @@
 <script setup>
 import { ref } from "vue";
+import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
 
-const todos = ref([]);
-const URL = "https://jsonplaceholder.typicode.com/todos";
+const articles = ref([]);
 
-async function dataFetch() {
+// 이 부분을 교체하세요
+const firebaseConfig = {
+  apiKey: "AIzaSyD4ArVfpfthN1tZVLrUGMK-iWDqMTCQDGk",
+  authDomain: "sample-fb558.firebaseapp.com",
+  projectId: "sample-fb558",
+  storageBucket: "sample-fb558.appspot.com",
+  messagingSenderId: "461305644484",
+  appId: "1:461305644484:web:70d80599b22bbf19bb5091",
+  measurementId: "G-ERTK6ZN0NM",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function fetchArtcles() {
   try {
-    const response = await fetch(URL, { method: "GET" });
-    const jsonData = await response.json();
-    todos.value = jsonData;
+    const querySnapshot = await getDocs(collection(db, "articles"));
+    articles.value = querySnapshot.docs.map((doc) => ({
+      articleId: doc.id,
+      ...doc.data(),
+    }));
+    console.log(articles.value);
+    0;
   } catch (error) {
     console.error(error);
   }
 }
 
-dataFetch();
+fetchArtcles();
 </script>
 
 <template>
-  <h1>Fetch API</h1>
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
+  <h1>게시판</h1>
+  <ul v-if="articles.length">
+    <li v-for="article in articles" :key="article.articleId">
+      <h2>{{ article.title }}</h2>
+      <p>{{ article.content }}</p>
+      <p>{{ article.date }}</p>
+    </li>
   </ul>
+  <p v-else>loading...</p>
 </template>
